61b965830c4672307be4a8b30d9da63812338072
abbasali361 <alabbashossain@gmail.com>
2021-04-07 18:43:02 +0600

    completely deploy in firebase

--- src/components/OrderDetails/OrderDetails.js
+++ src/components/OrderDetails/OrderDetails.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { UserContext } from '../../App';
 
 const OrderDetails = (props) => {
     const user = props.user;
     const { name, price, quality, imageURL, email } = user;
+    const [loggedInUser, setLoggedInUser]=useContext(UserContext)
     return (
         <div>
-
+            <div>
+                <h1>Hi... {loggedInUser.name} Welcome!!!!</h1>
+                <p>Email: {loggedInUser.email}</p>
+            </div>
             <Card >
                 <Card.Img variant="top" src={imageURL} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {quality}
+                    </Card.Text>
                     <Card.Text>
                         {price}
                     </Card.Text>
